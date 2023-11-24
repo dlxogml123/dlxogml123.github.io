fe1ffd7eec5fb2c18bdbc71f45eac5c20b06f5df
Regula Aurea <Acidosnow@gmail.com>
2023-11-24 20:33:34 +0900

    v 0.02
    네비게이션 바(이동 메뉴) 변경 및 완성 폰트 설정 완료

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
-// index.html의 하위페이지를 동적으로 로드하는 함수
-function loadSubpage() {
-    // subpageContent 요소를 가져와서 그 안에 subpage.html을 로드
-    var subpageContent = document.getElementById('subpageContent');
-    subpageContent.innerHTML = ''; // 기존 콘텐츠 초기화
+const Main_H1 = document.querySelector("#Main-h1");
 
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            subpageContent.innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'subpage.html', true);
-    xhr.send();
-}
-
-// 링크를 클릭할 때 loadSubpage 함수 호출
-document.querySelector('a[href="subpage.html"]').addEventListener('click', function(event) {
-    event.preventDefault(); // 링크 기본 동작 방지
-    loadSubpage();
+Main_H1.addEventListener("click", function () {
+  // console.log("hello");
+  location.href = "index.html";
 });
